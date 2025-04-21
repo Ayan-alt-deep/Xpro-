2f489e2788dd7e6f04c2290e01ec3afe7f5879d6
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-04-21 07:37:29 +0600

    Update session.js

--- session.js
+++ session.js
@@ -42,8 +42,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~XcEWUSjC#klPASNer2NPOf27mJwC8BVxiANvm_jPQq5E_k37AfcU",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
-    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "itachi-prime99",
+    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "8801839268235"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "session",
     FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
